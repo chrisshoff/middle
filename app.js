122c8dc4d3048f606a123fe210cb618426377733
Chris Shoff <chris.shoff@gmail.com>
2013-11-21 10:29:23 -0500

    Use heroku's PORT variable if available

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.get('/', routes.index);
 app.get('/maps', maps.lookupAddress);
 app.post('/maps', maps.meet);
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
